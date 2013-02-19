465c7e0aa519f94256fd2447eac66e5e3a171668
Raúl Naveiras <rnaveiras@gmail.com>
2013-02-19 18:20:19 +0100

    Disable the awfull message

--- lib/thinking_sphinx/auto_version.rb
+++ lib/thinking_sphinx/auto_version.rb
@@ -11,29 +11,6 @@ module ThinkingSphinx
         require 'riddle/2.0.1'
       when /2.0.[^12]/, /2.1.\d/
         require 'riddle/2.1.0'
-      else
-        documentation_link = %Q{
-For more information, read the documentation:
-http://freelancing-god.github.com/ts/en/advanced_config.html
-}
-
-        if version.nil? || version.empty?
-          STDERR.puts %Q{
-Sphinx cannot be found on your system. You may need to configure the following
-settings in your config/sphinx.yml file:
-  * bin_path
-  * searchd_binary_name
-  * indexer_binary_name
-
-#{documentation_link}
-}
-        else
-          STDERR.puts %Q{
-Unsupported version: #{version}
-
-#{documentation_link}
-}
-        end
       end
     end
   end
